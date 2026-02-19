37940e0bbbbb79365007d855b8fa6ec0c4d18dbb
JekabsPurins <s23purijeka@venta.lv>
2026-02-19 12:08:15 +0200

    Added resize function

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -53,6 +53,20 @@ public class MyArrayList {
 	
 	
 	
+	private void resize() {
+		int newSize = (howManyElements < 200 ) ? (size*2) : (int)(size * 1.5);
+		char[] newList = new char[newSize];
+		
+		for(int i = 0; i < howManyElements; i++) {
+			newList[i] = list[i];
+		}
+		list = newList;
+		size = newSize;
+		System.gc();
+	}
+	
+	
+	
 	
 	
 	
