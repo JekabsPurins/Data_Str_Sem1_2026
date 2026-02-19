e98ddcebd050f0a8dbc935378b08203e93f8beb9
JekabsPurins <s23purijeka@venta.lv>
2026-02-19 11:44:34 +0200

    Added constructors

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -8,6 +8,21 @@ public class MyArrayList {
 	private int size = DEFAULT_SIZE;
 	
 	
+	public MyArrayList() {
+		list = new char[size];
+	}
+	
+	
+	public MyArrayList(int inputSize) {
+		if(inputSize > 0 && inputSize < 100000) {
+			size = inputSize;
+		}		
+		list = new char[size];
+	}
+	
+	
+	
+	
 	
 	
 	
